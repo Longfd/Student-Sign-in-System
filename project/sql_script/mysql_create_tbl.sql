e1029a20bae07b89cd7550e28082c5ef7b342ae3
Longfd <longfeida.lfd@gmail.com>
2018-04-30 20:38:33 -0400

    sql adjustment

--- project/sql_script/mysql_create_tbl.sql
+++ project/sql_script/mysql_create_tbl.sql
@@ -17,34 +17,43 @@ s_id varchar(20) primary key, -- 学号
 s_name varchar(20) not null, -- 姓名
 cls_no varchar(20) not null, -- 班级号
 s_passwd varchar(20) not null, -- 密码
-constraint `fk_classid` foreign key(cls_no) references CLASS_TBL(cls_no)
+constraint `fk_STUClassid` foreign key(cls_no) references CLASS_TBL(cls_no)
 );
 
--- 4.课程表(教师创建二维码, 由老师提供课程编号, 课程名)
-create table COURSE_TBL(
-course_no varchar(20) primary key, -- 课程号
-course_name varchar(20)  not null, -- 课程名
-t_id varchar(20), -- 教师号
-constraint `fk_tid` foreign key(t_id) references TEACHER_TBL(t_id)
+-- 4.活动表(教师创建二维码, 由老师提供活动名), 活动号需要返回
+-- 主键自动增长, 插入时传入NULL即可
+create table ACTIVITY_TBL(
+act_no INT UNSIGNED AUTO_INCREMENT primary key, -- 活动号
+act_name varchar(20)  not null, -- 活动名
+t_id varchar(20) not null, -- 教师号
+act_class varchar(20) not null, -- 活动班级号
+constraint `fk_ATCTid` foreign key(t_id) references TEACHER_TBL(t_id)
 );
 
--- 5.选课表(学生扫码, 获取课程号并发送给后台, 
--- 后台判断, 首次选课添加到选课表并记录到签到表, 
--- 后续扫描, 只将记录添加到签到表)
-create table SEL_COURSE_TBL(
-s_id varchar(20)  not null, -- 学号
-course_no varchar(20) primary key, -- 课程号
-constraint `fk_SELStudentId` foreign key(s_id) references STUDENT_TBL(s_id)
-);
+-- 5.选课表
+-- create table SEL_COURSE_TBL(
+-- s_id varchar(20)  not null, -- 学号
+-- course_no varchar(20) primary key, -- 课程号
+-- constraint `fk_SELStudentId` foreign key(s_id) references STUDENT_TBL(s_id)
+-- );
 
 -- 6.签到表
--- 签到号自动增长, 插入时传入NULL即可
+-- 由教师创建活动时, 插入表数据
+-- 学生扫码, 根据(学号+活动号) 更新签到状态
 create table SINGIN_TBL(
 sign_id  INT UNSIGNED AUTO_INCREMENT primary key , -- 签到号
-course_no varchar(20)  not null, -- 课程号
-s_id varchar(20)  not null, -- 学号
+act_no INT UNSIGNED, -- 活动号
+act_name varchar(20), -- 活动名
+s_id varchar(20) not null, -- 学号
+s_name varchar(20), -- 学生姓名
+cls_no varchar(20) not null, -- 班级号
+cls_name varchar(20) not null, -- 名称
 sign_date varchar(15), -- 日期
 sign_time varchar(15), -- 时间
+sign_status INT, -- 签到状态(0-未签到, 1-已签到)
+constraint `fk_SIGNActivityNo` foreign key(act_no) references ACTIVITY_TBL(act_no),
 constraint `fk_SIGNStudentId` foreign key(s_id) references STUDENT_TBL(s_id),
-constraint `fk_SIGNCourseNo` foreign key(course_no) references COURSE_TBL(course_no)
+constraint `fk_SIGNClassid` foreign key(cls_no) references CLASS_TBL(cls_no)
 );
+
+commit;
