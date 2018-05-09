38ecfe945db9b5db748c2ada43590b7cd581ac3e
Longfd <longfeida.lfd@gmail.com>
2018-05-09 18:04:05 +0800

    sql mod

--- project/sql_script/mysql_create_tbl.sql
+++ project/sql_script/mysql_create_tbl.sql
@@ -3,22 +3,25 @@ create table TEACHER_TBL(
 t_id varchar(20) primary key, -- 教师号
 t_name varchar(20) not null, -- 姓名
 t_passwd varchar(20) not null
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- 2.班级表
 create table CLASS_TBL(
-cls_no varchar(20) primary key, -- 班级号
-cls_name varchar(20) not null -- 名称
-);
+cls_no INT UNSIGNED AUTO_INCREMENT primary key, -- 班级号
+cls_name varchar(20) not null, -- 名称
+t_id varchar(20)  not null, -- 教师号
+constraint `fk_TeacherId` foreign key(t_id) references TEACHER_TBL(t_id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- 3.学生表
 create table STUDENT_TBL(
 s_id varchar(20) primary key, -- 学号
 s_name varchar(20) not null, -- 姓名
-cls_no varchar(20) not null, -- 班级号
 s_passwd varchar(20) not null, -- 密码
+cls_no INT UNSIGNED, -- 班级号
+cls_name varchar(20), -- 班级名称
 constraint `fk_STUClassid` foreign key(cls_no) references CLASS_TBL(cls_no)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- 4.活动表(教师创建二维码, 由老师提供活动名), 活动号需要返回
 -- 主键自动增长, 插入时传入NULL即可
@@ -28,7 +31,7 @@ act_name varchar(20)  not null, -- 活动名
 t_id varchar(20) not null, -- 教师号
 act_class varchar(20) not null, -- 活动班级号
 constraint `fk_ATCTid` foreign key(t_id) references TEACHER_TBL(t_id)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- 5.选课表
 -- create table SEL_COURSE_TBL(
@@ -45,7 +48,7 @@ act_no INT UNSIGNED, -- 活动号
 act_name varchar(20), -- 活动名
 s_id varchar(20) not null, -- 学号
 s_name varchar(20), -- 学生姓名
-cls_no varchar(20) not null, -- 班级号
+cls_no INT UNSIGNED not null, -- 班级号
 cls_name varchar(20) not null, -- 名称
 sign_date varchar(15), -- 日期
 sign_time varchar(15), -- 时间
@@ -54,6 +57,6 @@ primary key(act_no, s_id),
 constraint `fk_SIGNActivityNo` foreign key(act_no) references ACTIVITY_TBL(act_no),
 constraint `fk_SIGNStudentId` foreign key(s_id) references STUDENT_TBL(s_id),
 constraint `fk_SIGNClassid` foreign key(cls_no) references CLASS_TBL(cls_no)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 commit;
